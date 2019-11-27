c2fca7dd3db71301523d681d442108c43c558344
fatfirebird <fatfirebird@gmail.com>
2019-11-27 15:05:51 +0500

    add cors

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ const express = require('express');
 const mongoose = require('mongoose');
 const usersRoute = require('./routes/users');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv/config');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/users', usersRoute);
-
 
 mongoose.connect(
    process.env.MONGODB_CONNECTION, {
      useNewUrlParser: true,
      useUnifiedTopology: true
    }
-);
+)
 
 app.get('/', (req, res) => {
   res.send('Старт');
 });
 
-app.listen(3000);
+app.listen(8000);
